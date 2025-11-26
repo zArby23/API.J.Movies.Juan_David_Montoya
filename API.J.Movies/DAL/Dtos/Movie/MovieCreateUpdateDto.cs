af3c97d6289f66739e3d0bf6de6722930277bcae
Juan Montoya <lic.jdavid23@gmail..com>
2025-11-26 14:07:08 -0500

    Created Movie DTOs

---
+++ API.J.Movies/DAL/Dtos/Movie/MovieCreateUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Dtos.Movie
+{
+    public class MovieCreateUpdateDto
+    {
+        [Required(ErrorMessage = "El nombre de la película es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El nombre de la película no puede exceder los 100 caracteres")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "La duración de la película es obligatoria.")]
+        public int Duration { get; set; } //en minutos
+        [MaxLength(500, ErrorMessage = "La descripción no puede exceder los 500 caracteres")]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "La clasificación de la película es obligatoria.")]
+        [MaxLength(10, ErrorMessage = "La clasificación no puede exceder los 10 caracteres.")]
+        public string Clasification { get; set; }
+    }
+}
---
+++ API.J.Movies/DAL/Dtos/Movie/MovieDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.J.Movies.DAL.Dtos.Movie
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la película es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El nombre de la película no puede exceder los 100 caracteres")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "La duración de la película es obligatoria.")]
+        public int Duration { get; set; } //en minutos
+        [MaxLength(500, ErrorMessage = "La descripción no puede exceder los 500 caracteres")]
+        public string? Description { get; set; }
+        [Required(ErrorMessage = "La clasificación de la película es obligatoria.")]
+        [MaxLength(10, ErrorMessage = "La clasificación no puede exceder los 10 caracteres.")]
+        public string Clasification { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
