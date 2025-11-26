c3486a92c621578d2f719082b50b0cfa6af9f78f
Juan Montoya <lic.jdavid23@gmail..com>
2025-11-26 14:06:38 -0500

    Created Movie Repostory, inheriting the methods from the interface IMovieRepository

---
+++ API.J.Movies/Repository/MovieRepository.cs
@@ -0,0 +1,75 @@
+﻿using API.J.Movies.DAL;
+using API.J.Movies.DAL.Models;
+using API.J.Movies.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.J.Movies.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MovieRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> MovieExistsByIdAsync(int id)
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .AnyAsync(m => m.Id == id);
+        }
+
+        public async Task<bool> MovieExistsByNameAsync(string name)
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .AnyAsync(m => m.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+        public async Task<bool> CreateMovieAsync(Movie movie)
+        {
+            movie.CreatedDate = DateTime.UtcNow;
+            await _context.Movies.AddAsync(movie);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteMovieAsync(int id)
+        {
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie == null)
+                return false;
+
+            _context.Movies.Remove(movie);
+            return await SaveAsync();
+        }
+
+        public async Task<Movie> GetMovieAsync(int id)
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<ICollection<Movie>> GetMoviesAsync()
+        {
+            return await _context.Movies
+                .AsNoTracking()
+                .OrderBy(m => m.Name)
+                .ToListAsync();
+        }
+
+        public async Task<bool> UpdateMovieAsync(Movie movie)
+        {
+            movie.ModifiedDate = DateTime.UtcNow;
+            _context.Movies.Update(movie);
+            return await SaveAsync();
+        }
+        private async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() >= 0 ? true : false;
+        }
+    }
+}
