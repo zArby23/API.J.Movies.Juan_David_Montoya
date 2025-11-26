549ff954a3627c3082752e6c9ba5ba31a734b3eb
Juan Montoya <lic.jdavid23@gmail..com>
2025-11-26 14:09:54 -0500

    Implemented Movies builders on the main program, making the validation and verification possible on Swagger.

--- API.J.Movies/Program.cs
+++ API.J.Movies/Program.cs
@@ -14,9 +14,11 @@ builder.Services.AddAutoMapper(cfg => cfg.AddProfile<Mappers>());
 
 //Services
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IMovieService, MovieService>();
 
 //Repositories
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
