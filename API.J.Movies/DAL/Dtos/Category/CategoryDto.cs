2dc90e448ed2c3ea01990e854bfa4efd95f3d8bf
Juan Montoya <lic.jdavid23@gmail..com>
2025-11-26 14:04:17 -0500

    Minor change

--- API.J.Movies/DAL/Dtos/Category/CategoryDto.cs
+++ API.J.Movies/DAL/Dtos/Category/CategoryDto.cs
@@ -6,8 +6,8 @@ namespace API.J.Movies.DAL.Dtos.Category
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El nombre de la categoría es obligatorio")]
-        [MaxLength(100, ErrorMessage = "El nombre de la categoría no puede exceder los 100 caracteres")]
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El nombre de la categoría no puede exceder los 100 caracteres.")]
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? ModifiedDate { get; set; }
